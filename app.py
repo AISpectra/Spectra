ea667fa32cf098e9d5d8d7f72aa29aac3bbb5024
Samuel <spectra.gethelp@gmail.com>
2025-02-12 19:49:26 +0100

    Actualizacion

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from supabase import create_client, Client
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-if not SUPABASE_URL or not SUPABASE_SERVICE_ROLE_KEY
+if not SUPABASE_URL or not SUPABASE_SERVICE_ROLE_KEY:
     raise ValueError("Las credenciales de Supabase no están configuradas correctamente.")
 
 # Crear cliente de Supabase
